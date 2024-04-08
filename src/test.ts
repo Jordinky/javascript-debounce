3760ceccc901640498d73650f86d9a6126044e3d
Jordi Pelaez <jordipelaez05@gmail.com>
2024-04-08 12:04:14 +0200

    test implementados

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,13 @@
 import { debounce } from "./debounce";
 import { memoize } from "./memoize";
+import { throttle } from  "./throttle"
 
 var counter = 0;
 const update =()=>{
     counter ++
 }
 const deb = debounce(update);
+const thrott = throttle(update)
 
 test("debounce function testing",()=>{
     expect(deb)
@@ -16,4 +18,8 @@ const memo = memoize (factorial);
 
 test("memoize function testing",()=>{
     expect(memo(6));
+})
+
+test("throttle function testing",()=>{
+    expect(thrott)
 })
